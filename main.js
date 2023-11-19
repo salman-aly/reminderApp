d63b3086e0b2260a7d4152013b26b1b0fd6339c2
salman <alysalman15@gmail.com>
2023-11-19 18:41:34 +0500

    task reminder

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+let Div = document.querySelector(".namaz-time");
+let div = document.querySelector(".task-container");
+
 const time = moment().format('LTS');
 const date = moment().format('LL');
+
+const now = new Date();
+const targetDate = new Date();
 
 let clockTime = document.getElementById("time");
 let clockDate = document.getElementById("date");
 let dateClock = document.getElementById("date-js")
+
+const currentTimeString = now.toLocaleTimeString('en-US', { hour12: true });
+const targetTimeString = targetDate.toLocaleTimeString('en-US', { hour12: true });
 
 
 setInterval(() => {
@@ -22,8 +31,6 @@ dateClock.innerHTML = `
   <div><h2>${date}</h2></div>
 `;
 
-let Div = document.querySelector(".namaz-time");
-let div = document.querySelector(".task-container");
 
 function hideNamz() {
   let hide = document.querySelector(".namaz-time");
@@ -40,28 +47,27 @@ function hideTask() {
 }
 
 
-const now = new Date();
-const targetDate = new Date();
-
-// Set the target time to 2:06:40 PM
 targetDate.setHours(2, 11, 40, 0);
 
-// Format the current time and target time to string in 12-hour format
-const currentTimeString = now.toLocaleTimeString('en-US', { hour12: true });
-const targetTimeString = targetDate.toLocaleTimeString('en-US', { hour12: true });
-
 if (now.getTime() === targetDate.getTime()) {
-  alert(`Current time (${currentTimeString}) is equal to or later than target time (${targetTimeString}). Wake up!`);
+  alert("Wake up");
 }
 
 
-let post = document.getElementById("postedPost");
 
 function createPost() {
-  let posted = document.getElementById("postedPost");
-  posted.innerHTML += `
-  <h4>${post.value}</h4>
- `
+  let userTime = document.getElementById("timedate")
+
+  let post = document.getElementById("inputValue");
+  let postContent = document.getElementById("postedPost")
+
+  postContent.innerHTML += `
+   <div class="postedPost-container">
+     <h4>${post.value}</h4>
+     <h4>${userTime.value}</h4>
+    </div>
+  `;
+  post.value = ""
 }
 
 
