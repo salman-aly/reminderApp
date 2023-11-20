ee5013b8e60c0ea5d3d8dfc0e123a94d115ce1b6
salman <alysalman15@gmail.com>
2023-11-20 12:11:53 +0500

    pwa complete

--- app.js
+++ app.js
@@ -125,3 +125,4 @@ function forgetBtnPopup() {
     })
 
 }
+
--- serviceworker.js
+++ serviceworker.js
@@ -1,37 +1,19 @@
 var staticCacheName = "pwa";
-
+ 
 self.addEventListener("install", function (e) {
-    e.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(["/"]);
-        })
-    );
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
+    })
+  );
 });
-
+ 
 self.addEventListener("fetch", function (event) {
-    console.log(event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-
-//for push notifcation
-self.addEventListener('push', function (event) {
-    const options = {
-        body: event.data.text(),
-        icon: 'icon.png',
-        badge: 'badge.png',
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', options)
-    );
-});
-
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
